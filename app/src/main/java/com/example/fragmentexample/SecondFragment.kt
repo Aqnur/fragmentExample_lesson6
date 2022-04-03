206c7cbe86a7785d8d7da8b84230e27169af0499
Aqnur <aknurr999@gmail.com>
2022-04-03 15:37:38 +0600

    changes

--- app/src/main/java/com/example/fragmentexample/SecondFragment.kt
+++ app/src/main/java/com/example/fragmentexample/SecondFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.fragmentexample.databinding.ActivityMainBinding
 import com.example.fragmentexample.databinding.FragmentSecondBinding
 
@@ -18,5 +19,17 @@ class SecondFragment : Fragment() {
     ): View {
         binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogin.setOnClickListener {
+            if (!binding.etLogin.text.isNullOrBlank()) {
+                Toast.makeText(requireContext(), "is not null", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(requireContext(), "is null", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
